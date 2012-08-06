83eb0b37f04ad46ff8e647030441b3c49ecc0a57
Tavis Rudd <tavis@damnsimple.com>
2012-08-05 20:31:28 -0700

    git / hitch / magit integration

--- dss-vc.el
+++ dss-vc.el
@@ -48,4 +48,30 @@
     (when backend
       (vc-state-refresh file backend))))
 
+;;;
+(defun dss/git-rehitch ()
+  (interactive)
+  (let ((hitch-status
+         (apply #'format
+                (cons "git hitch: %s - %s"
+                      (mapcar
+                       (lambda (var)
+                         (setenv var "")
+                         (let ((val (dss/local-shell-command-to-string
+                                     (concat "echo -n $" var))))
+                           (setenv var val)
+                           val))
+                       '("GIT_AUTHOR_NAME" "GIT_AUTHOR_EMAIL"))))))
+    (message hitch-status)
+    hitch-status))
+
+(defun dss/magit-status-hook ()
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (insert (dss/git-rehitch))
+    (insert "\n")))
+
+(add-hook 'magit-refresh-status-hook 'dss/magit-status-hook)
+
 (provide 'dss-vc)
